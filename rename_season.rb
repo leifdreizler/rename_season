308b9b7609f6f50f9d3e41c65038749127baa534
Leif <leifdreizler@gmail.com>
2016-01-13 06:27:19 -0800

    Ruby-ified the spacing

--- rename_season.rb
+++ rename_season.rb
@@ -4,104 +4,100 @@ require 'net/http'
 require 'json'
 
 def get_season_info(id, season)
-	source='http://www.omdbapi.com/?i=' + id + '&Season=' + season
-	
-	url = URI.parse(source)
-	req = Net::HTTP::Get.new(url.to_s)
-	res = Net::HTTP.start(url.host, url.port) {|http|http.request(req)}
-	return res.body
+  source='http://www.omdbapi.com/?i=' + id + '&Season=' + season
+  
+  url = URI.parse(source)
+  req = Net::HTTP::Get.new(url.to_s)
+  res = Net::HTTP.start(url.host, url.port) {|http|http.request(req)}
+  return res.body
 end
 
 def format_season_info(json)
-	blob = JSON.parse(json)	
-	title = blob["Title"]
-	season = blob["Season"]
-	
-	formatted_season_info = []
-	blob["Episodes"].each do |i|
-		formatted_season_info << title_builder(title, season, i["Episode"], i["Title"])
-	end
-	
-	return formatted_season_info
+  blob = JSON.parse(json)  
+  title = blob["Title"]
+  season = blob["Season"]
+  
+  formatted_season_info = []
+  blob["Episodes"].each do |i|
+    formatted_season_info << title_builder(title, season, i["Episode"], i["Title"])
+  end
+  
+  return formatted_season_info
 end
 
-def rename_file(original_file, episode_title)	
-	new_filename = episode_title + quality(original_file) + File.extname(original_file)
-	path_and_file = File.rename(original_file, File.dirname(original_file) + "/" + new_filename.gsub(/:/,''))
-	return path_and_file
+def rename_file(original_file, episode_title)  
+  new_filename = episode_title + quality(original_file) + File.extname(original_file)
+  path_and_file = File.rename(original_file, File.dirname(original_file) + "/" + new_filename.gsub(/:/,''))
+  return path_and_file
 end
-	
+  
 def check_rename(files, episode_array)
-	total_files = files.zip(episode_array).length
-	matched_files = 0
+  total_files = files.zip(episode_array).length
+  matched_files = 0
 
-	files.zip(episode_array).each do |original_file, episode_title|
-		new_filename = episode_title + quality(original_file) + File.extname(original_file)
-		path_and_file = File.dirname(original_file) + "/" + new_filename.gsub(/:/,'')
+  files.zip(episode_array).each do |original_file, episode_title|
+    new_filename = episode_title + quality(original_file) + File.extname(original_file)
+    path_and_file = File.dirname(original_file) + "/" + new_filename.gsub(/:/,'')
 
-		if new_filename.match(/(s\d\de\d\d)/i).to_s == (path_and_file.match(/(s\d\de\d\d)/i)).to_s
-	 		matched_files += 1
-		end
+    if new_filename.match(/(s\d\de\d\d)/i).to_s == (path_and_file.match(/(s\d\de\d\d)/i)).to_s
+       matched_files += 1
+    end
 
-		puts "proposed rename... \n" + original_file + "\n" + path_and_file + "\n"
-	end
+    puts "proposed rename... \n" + original_file + "\n" + path_and_file + "\n"
+  end
 
-	puts total_files.to_s + " checked, " + matched_files.to_s + " matched (should be equal)"
-	print "Everything look ok? "
-	answer = gets.chomp
+  puts total_files.to_s + " checked, " + matched_files.to_s + " matched (these numbers should be equal, if not consider abort)"
+  print "Type yes if everything looks ok: "
+  answer = gets.chomp
 
-	if answer.to_s != "yes"
-		exit
-	end
+  if answer.to_s != "yes"
+    exit
+  end
 end
-
 
 def title_builder(show, season, episode_number, episode_name)
-         filename = show + ' S' + pad(season) + 'E' + pad(episode_number) + ' ' + episode_name
-	 return filename
+  filename = show + ' S' + pad(season) + 'E' + pad(episode_number) + ' ' + episode_name
+  return filename
 end
 
 def pad(number)
-	num = number.to_i
-	if num < 10 
-		return "0" + number
-	else
-		return number
-	end
-end	
+  num = number.to_i
+  if num < 10 
+    return "0" + number
+  else
+    return number
+  end
+end  
 
 def quality(file)
-	if file.include? "480p"
-		return " [480p]"
-	elsif file.include? "576p"
-		return " [576p]"
-	elsif file.include? "720p"
-		return " [720p]"
-	elsif file.include? "1080p"
-		return " [1080p]"
-	else
-		return ""
-	end
+  if file.include? "480p"
+    return " [480p]"
+  elsif file.include? "576p"
+    return " [576p]"
+  elsif file.include? "720p"
+    return " [720p]"
+  elsif file.include? "1080p"
+    return " [1080p]"
+  else
+    return ""
+  end
 end
 
 
 
 def season_iterator(source_dir, episode_array)
-    files = Dir.glob(source_dir + "/*").sort_by{|word| word.downcase}
+  files = Dir.glob(source_dir + "/*").sort_by{|word| word.downcase}
+  
+  check_rename(files, episode_array)
 
-    check_rename(files, episode_array)
-
-    files.zip(episode_array).each do |file, episode|
-        rename_file(file, episode)
-    end
-       
+  files.zip(episode_array).each do |file, episode|
+    rename_file(file, episode)
+  end       
 end
 
 
-
-
-source_dir = #string to directory 
-imdb_id =  #string of IMDB ID, the part after title in: http://www.imdb.com/title/tt0472954 
+source_dir = #string to directory, don't put trailing /
+imdb_id = #string of IMDB ID, the part after title in: http://www.imdb.com/title/tt0472954 
 season_number = #string of season number
 
 season_info = get_season_info(imdb_id, season_number)
