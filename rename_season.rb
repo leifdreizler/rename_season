7462c6d129b329dee77d777815e914fdbc0473e8
Leif <leifdreizler@gmail.com>
2016-09-28 16:29:10 -0700

    Added the ability to do multiple seasons of the same show at the same time

--- rename_season.rb
+++ rename_season.rb
@@ -50,7 +50,7 @@ def check_rename(files, episode_array)
     puts "proposed rename... \n" + original_file + "\n" + path_and_file + "\n"
   end
 
-  puts total_files.to_s + " checked, " + matched_files.to_s + " matched (these numbers should be equal, if not consider abort)"
+  puts "\n" + total_files.to_s + " checked, " + matched_files.to_s + " matched (these numbers should be equal, if not consider abort)"
   print "Type yes if everything looks ok: "
   answer = gets.chomp
 
@@ -113,16 +113,47 @@ def parse_seasons(seasons)
 end
 
 def iterate_seasons(seasons_list, imdb_id, source_dir)
-  if seasons_list.length == 1
-    season_info = get_season_info(imdb_id, seasons_list[0])
-    season_array = format_season_info(season_info)
-    season_iterator(source_dir, season_array)
+ if seasons_list.length == 1
+   season_info = get_season_info(imdb_id, seasons_list[0])
+   season_array = format_season_info(season_info)
+   season_iterator(source_dir, season_array)
+ else
+   if seasons_list.length != num_seasons(source_dir)
+     puts "number of season folders didn't match the number of seasons to parse from IMDB"
+     exit
+   else
+    files = Dir.glob(source_dir + "/*").sort_by{|word| word.downcase}
+
+    puts "proposed matching..."
+    files.zip(seasons_list).each do |season_folder, season_number|
+      puts season_number + " to " + season_folder + "\n"
+    end 
+   
+    print "Type yes if everything looks ok: "
+    answer = gets.chomp
+
+    if answer.to_s != "yes"
+      exit
+    end
+
+    files.each_with_index do |season, index|
+      season_info = get_season_info(imdb_id, seasons_list[index])
+      season_array = format_season_info(season_info)
+      season_iterator(season, season_array)
+    end
+
   end
+ end
+
 end
 
-source_dir = "/Volumes/Death Star/TV Shows/Mr. Robot/"#string to directory, don't put trailing /
-imdb_id = "tt4158110" #string of IMDB ID, the part after title in: http://www.imdb.com/title/tt0472954 
-seasons = "1, 2" #string of season number. 2, 3, 4-5, or just a single season. 
+def num_seasons(source_dir)
+ return Dir.glob(source_dir + "/*").select {|f| File.directory? f}.length
+end
+
+source_dir = ""#string to directory, don't put trailing /
+imdb_id = "" #string of IMDB ID, the part after title in: http://www.imdb.com/title/tt0472954 
+seasons = "" #string of season number. 2, 3, 4-5, or just a single season. 
 #If doing multiple seasons, you need to have the source_dir be a dir of dirs
 
 iterate_seasons(parse_seasons(seasons), imdb_id, source_dir)
